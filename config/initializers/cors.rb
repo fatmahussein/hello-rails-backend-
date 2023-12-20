3b592c7d3b2cb2b15aaa00b42dcc64d1cf7be14b
Fatma Hussein <fartunhussein346@gmail.com>
2023-12-20 12:04:54 +0300

    add cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,12 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://127.0.0.1:3001', 'http://localhost:3000'
+      resource '/api/*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+  end
